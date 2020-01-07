0093e51f17f3315721c7366f11f112b46750b7e5
S. Makai Takori <makait87@gmail.com>
2020-01-07 15:58:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,14 @@
 class Person
-  def 
-
-
-
-
+  def name=(full_name)
+    @name = full_name
+  end  
+  def name
+    @name
+  end
+  def job=(job_title)
+    @job = job_title
+  end  
+  def job
+    @job
+  end  
 end  
